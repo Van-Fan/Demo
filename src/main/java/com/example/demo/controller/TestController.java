1cab58a3a1a9ce0c827300d2c12de5246a797aba
p10325981 <p10325981@P10325981.pic.com.tw>
2019-08-07 13:38:01 +0800

    init

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	@RequestMapping(value = "/index")
+	public String index(){
+		
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		
+		return "Hello , "+sdf.format(now);
+	}
+	
+	
+
+}
